deaa027d699c39aca0c6872868e9f557969be7e9
Fantin <fantin@malou.io>
2021-02-04 19:09:16 +0100

    show phpinsights details

---
+++ src/Database/Ugly.php
@@ -0,0 +1,24 @@
+<?php
+
+class   Ugly{
+    private $_ugly;
+
+    function __construct() {
+    }
+
+    
+
+      private function test() {
+        $arr =  array();
+        echo 'test';
+    }
+
+
+
+    public function TOTO() {
+        echo 'toto';
+    }
+
+
+
+}
